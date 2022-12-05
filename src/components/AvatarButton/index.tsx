8386ce1b75460e977368129066a616c177a040e1
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-05 16:43:01 -0500

    feat: add avatar component

---
+++ src/components/AvatarButton/index.tsx
@@ -0,0 +1,48 @@
+import { type MouseEvent } from 'react'
+import { getAvatarProps, getIconProps } from '@pluralsight/headless-styles'
+import { PersonIcon } from '@pluralsight/icons'
+import { type AvatarOptions } from '@pluralsight/headless-styles/types'
+
+type AvatarReturn = ReturnType<typeof getAvatarProps>
+
+function MatchAvatarContent(props: Omit<AvatarReturn, 'wrapper'>) {
+  if (props.image.src) {
+    return <AvatarImg {...props.image} />
+  } else if (props.label['aria-label']) {
+    return <AvatarLabel {...props.label} />
+  } else {
+    return <PersonIcon {...getIconProps(props.iconOptions)} />
+  }
+}
+
+type AvatarImgProps = Pick<AvatarReturn['image'], keyof AvatarReturn['image']>
+function AvatarImg(props: AvatarImgProps) {
+  return <img {...props} />
+}
+
+type AvatarLabelProps = Pick<AvatarReturn['label'], keyof AvatarReturn['label']>
+function AvatarLabel(props: AvatarLabelProps) {
+  const { value, ...labelProps } = props
+  return <div {...labelProps}>{value}</div>
+}
+
+interface AvatarButtonProps extends AvatarOptions {
+  onClick?: (event: MouseEvent) => void | (() => void)
+}
+
+export default function AvatarButton(props: AvatarButtonProps) {
+  const { wrapper, ...avatarProps } = getAvatarProps({
+    label: props.label || '',
+    src: props.src || '',
+    sentiment: props.sentiment,
+    size: props.size,
+  })
+
+  return (
+    <>
+      <button {...wrapper} onClick={props.onClick}>
+        <MatchAvatarContent {...avatarProps} />
+      </button>
+    </>
+  )
+}
