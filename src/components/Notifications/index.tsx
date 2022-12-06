274b4227e86c3d6b3225c10a42ff3bada90ded37
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-06 15:28:17 -0500

    refactor: make notifications slightly more dynamic and include close button handler

--- src/components/Notifications/index.tsx
+++ src/components/Notifications/index.tsx
@@ -1,3 +1,4 @@
+import { type MouseEvent } from 'react'
 import { getIconButtonProps, getIconProps } from '@pluralsight/headless-styles'
 import { type IconOptions } from '@pluralsight/headless-styles/types'
 import {
@@ -22,94 +23,88 @@ const notificationIconButtonProps = getIconButtonProps({
   size: 'l',
 })
 
+const notificationData = {
+  notificationList: [
+    'notification-1',
+    'notification-2',
+    'notification-3',
+    'notification-4',
+    'notification-5',
+    'notification-6',
+    'notification-7',
+  ],
+  notificationDetails: {},
+}
+
 export default function Notifications() {
+  function handleClose(event: MouseEvent) {
+    event.preventDefault()
+  }
+
   return (
-    <div className={styles.notificationWrapper}>
-      <Menu
-        label="Notifications"
-        trigger={
-          <button {...iconButtonProps.button}>
-            <NotificationsActiveIcon
-              {...getIconProps(iconButtonProps.iconOptions as IconOptions)}
-            />
-          </button>
-        }
-        style={{
-          right: '0',
-          width: '23rem',
-          maxHeight: '75vh',
-          overflowY: 'auto',
-        }}
-      >
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem>
-          <Notification />
-        </MenuItem>
-        <MenuItem divider={true} />
-        <MenuItem href="/">View all notifications</MenuItem>
-      </Menu>
-    </div>
+    <Menu
+      label="Notifications"
+      trigger={
+        <button {...iconButtonProps.button}>
+          <NotificationsActiveIcon
+            {...getIconProps(iconButtonProps.iconOptions as IconOptions)}
+          />
+        </button>
+      }
+      style={{
+        right: '0',
+        width: '23rem',
+        maxHeight: '75vh',
+        overflowY: 'auto',
+      }}
+    >
+      {notificationData.notificationList.map((datum) => (
+        <Notification handleClose={handleClose} />
+      ))}
+      <MenuItem divider={true} />
+      <MenuItem href="/">View all notifications</MenuItem>
+    </Menu>
   )
 }
 
-function Notification() {
+interface NotificationProps {
+  handleClose: (event: MouseEvent) => void
+}
+
+function Notification(props: NotificationProps) {
   return (
-    <div className={styles.notification}>
-      <span className={styles.notificationCloseButton}>
-        <button {...notificationIconButtonProps.button}>
-          <CloseIcon
-            {...getIconProps(
-              notificationIconButtonProps.iconOptions as IconOptions
-            )}
-          />
-        </button>
-      </span>
-      <a href="/" className={styles.notificationContent}>
-        <span className={styles.notificationIcon}>
-          <MegaphoneIcon {...getIconProps({ size: 'l', ariaHidden: true })} />
-        </span>
-        <div className={styles.notificationBody}>
-          <p>
-            A new course came out on{' '}
-            <strong>React: Using Hooks in React 18</strong>
-          </p>
-          <time
-            dateTime="2022-12-02T20:48:18.630Z"
-            className={styles.notificationTimestamp}
+    <MenuItem href="/">
+      <div className={styles.notification}>
+        <span className={styles.notificationCloseButton}>
+          <button
+            {...notificationIconButtonProps.button}
+            onClick={props.handleClose}
           >
-            About 1 month ago
-          </time>
-        </div>
-      </a>
-    </div>
+            <CloseIcon
+              {...getIconProps(
+                notificationIconButtonProps.iconOptions as IconOptions
+              )}
+            />
+          </button>
+        </span>
+        <span className={styles.notificationContent}>
+          <span className={styles.notificationIcon}>
+            <MegaphoneIcon {...getIconProps({ size: 'l', ariaHidden: true })} />
+          </span>
+          <div className={styles.notificationBody}>
+            <p>
+              A new course came out on{' '}
+              <strong>React: Using Hooks in React 18</strong>
+            </p>
+            <time
+              dateTime="2022-12-02T20:48:18.630Z"
+              className={styles.notificationTimestamp}
+            >
+              About 1 month ago
+            </time>
+          </div>
+        </span>
+      </div>
+    </MenuItem>
   )
 }
