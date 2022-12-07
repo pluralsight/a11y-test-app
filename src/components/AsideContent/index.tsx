9cc05eef9078231f396b385f1355438f02cd8b57
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-07 13:26:12 -0500

    feat: add weekly goal placeholder component

--- src/components/AsideContent/index.tsx
+++ src/components/AsideContent/index.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from 'react'
 import RoleIqCard from '../RoleIqCard'
+import WeeklyGoal from '../WeeklyGoal'
 import sytles from './AsideContent.module.css'
 
 export default function AsideContent(props: PropsWithChildren) {
   return (
     <div className={sytles.asideContent}>
       <RoleIqCard />
+      <WeeklyGoal />
     </div>
   )
 }
---
+++ src/components/WeeklyGoal/index.tsx
@@ -0,0 +1,31 @@
+import { getButtonProps } from '@pluralsight/headless-styles'
+import Card from '../Card'
+import CardHeader from '../CardHeader'
+import Flex from '../Flex'
+import Progress from '../Progress'
+
+export default function WeeklyGoal() {
+  return (
+    <Card>
+      <Flex justify="space-between">
+        <CardHeader>Weekly goal</CardHeader>
+        <button
+          {...getButtonProps({
+            usage: 'outline',
+            size: 'm',
+          }).button}
+        >
+          Edit goal
+        </button>
+      </Flex>
+      <span>
+        <big>
+          <b>0 min</b>
+        </big>{' '}
+        / 30 min
+      </span>
+      <Progress now={0} />
+      <small>Completed this week</small>
+    </Card>
+  )
+}
