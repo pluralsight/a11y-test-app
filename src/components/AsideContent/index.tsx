905bc9c166657cd04d561371a9b422e77b72919e
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-07 13:27:51 -0500

    refactor: rename WeeklyGoal to WeeklyGoalCard

--- src/components/AsideContent/index.tsx
+++ src/components/AsideContent/index.tsx
@@ -1,13 +1,13 @@
 import { PropsWithChildren } from 'react'
 import RoleIqCard from '../RoleIqCard'
-import WeeklyGoal from '../WeeklyGoal'
+import WeeklyGoalCard from '../WeeklyGoalCard'
 import sytles from './AsideContent.module.css'
 
 export default function AsideContent(props: PropsWithChildren) {
   return (
     <div className={sytles.asideContent}>
       <RoleIqCard />
-      <WeeklyGoal />
+      <WeeklyGoalCard />
     </div>
   )
 }
