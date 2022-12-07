da9d9289aeba804f2e6a39ce047af9d664561ea7
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-07 16:16:50 -0500

    refactor: rename GetStartedCard to HistoryCard and remove unused css

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,11 +1,11 @@
 import { PropsWithChildren } from 'react'
-import GetStartedCard from '../GetStartedCard'
+import HistoryCard from '../HistoryCard'
 import styles from './MainContent.module.css'
 
 export default function MainContent(props: PropsWithChildren) {
   return (
     <div className={styles.mainContent}>
-      <GetStartedCard />
+      <HistoryCard />
     </div>
   )
 }
