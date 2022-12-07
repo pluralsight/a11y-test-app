4688a83b0771616ebb68d44f02a0193789c8f87b
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-07 17:22:05 -0500

    fix: remove unused styles from main content

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,7 +1,6 @@
 import { getGridItemProps } from '@pluralsight/headless-styles'
 import { PropsWithChildren } from 'react'
 import HistoryCard from '../HistoryCard'
-import styles from './MainContent.module.css'
 
 export default function MainContent(props: PropsWithChildren) {
   return (
