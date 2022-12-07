84d386cd2fd3ade8584ce6a0ed031fded7094d1b
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-07 13:20:17 -0500

    feat: add card header component

---
+++ src/components/CardHeader/index.tsx
@@ -0,0 +1,36 @@
+import { getIconProps } from '@pluralsight/headless-styles'
+import { ChevronRightIcon } from '@pluralsight/icons'
+import { PropsWithChildren } from 'react'
+import Flex from '../Flex'
+import TextLink from '../TextLink'
+import styles from './CardHeader.module.css'
+
+interface CardHeaderProps extends PropsWithChildren {
+  linkText?: string
+  linkIconText?: string
+  linkHref?: string
+}
+
+export default function CardHeader(props: CardHeaderProps) {
+  return (
+    <header className={styles.header}>
+      <Flex align="center">
+        <h2 className={styles.heading}>{props.children}</h2>
+        {props.linkHref && (
+          <TextLink href={props.linkHref}>
+            <Flex align="center" justify="space-between">
+              {props.linkText}{' '}
+              <ChevronRightIcon
+                {...getIconProps({
+                  size: 's',
+                  ariaLabel: props.linkIconText,
+                  ariaHidden: !props.linkIconText,
+                })}
+              />
+            </Flex>
+          </TextLink>
+        )}
+      </Flex>
+    </header>
+  )
+}
