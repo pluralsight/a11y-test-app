cd419e71020263c54bba4d729c0d9c5e065b33a0
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-08 11:14:06 -0500

    feat: add required label for progress bar

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,12 +1,17 @@
 import { getProgressProps } from '@pluralsight/headless-styles'
 import { type ProgressOptions } from '@pluralsight/headless-styles/types'
 
-export default function Progress(props: ProgressOptions) {
-  const progressProps = getProgressProps(props)
+interface ProgressProps extends ProgressOptions {
+  label: string
+}
+
+export default function Progress(props: ProgressProps) {
+  const { label, ...progressOptions } = props
+  const progressProps = getProgressProps(progressOptions)
 
   return (
     <div {...progressProps.wrapper}>
-      <div {...progressProps.bar} />
+      <div {...progressProps.bar} aria-label={label} />
     </div>
   )
 }
