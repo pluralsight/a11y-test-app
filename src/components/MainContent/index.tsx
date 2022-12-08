8e42427f50fc4aec3e5f5b988851650713604c69
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-08 08:08:49 -0500

    refactor: move vertical rhythm control to parent containers

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -2,12 +2,15 @@ import { getGridItemProps } from '@pluralsight/headless-styles'
 import { PropsWithChildren } from 'react'
 import HistoryCard from '../HistoryCard'
 import TechFoundationsCard from '../TechFoundationsCard'
+import styles from './MainContent.module.css'
 
 export default function MainContent(props: PropsWithChildren) {
   return (
     <div {...getGridItemProps({ colSpan: 7 })}>
+      <div className={styles.mainContent}>
       <HistoryCard />
       <TechFoundationsCard />
+      </div>
     </div>
   )
 }
