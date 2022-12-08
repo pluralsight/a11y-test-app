474334832d85d1036ab33c6d18a00069d4261605
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-08 07:04:58 -0500

    feat: add tech foundations card

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,11 +1,13 @@
 import { getGridItemProps } from '@pluralsight/headless-styles'
 import { PropsWithChildren } from 'react'
 import HistoryCard from '../HistoryCard'
+import TechFoundationsCard from '../TechFoundationsCard'
 
 export default function MainContent(props: PropsWithChildren) {
   return (
     <div {...getGridItemProps({ colSpan: 7 })}>
       <HistoryCard />
+      <TechFoundationsCard />
     </div>
   )
 }
