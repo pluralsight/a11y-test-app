906528178e7997d427130161fb027dbde63e3910
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-09 15:12:55 -0500

    feat: add radio component

---
+++ src/components/Form/Radio.tsx
@@ -0,0 +1,26 @@
+import { getFormLabelProps, getRadioProps } from '@pluralsight/headless-styles'
+import { type RadioOptions } from '@pluralsight/headless-styles/types'
+import { type ChangeEvent } from 'react'
+
+interface RadioProps extends RadioOptions {
+  label: string
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function BasicRadio(props: RadioProps) {
+  const { onChange, direction, ...radioOptions } = props
+  const radioProps = getRadioProps(radioOptions)
+  const { value, ...labelProps } = getFormLabelProps({
+    ...radioOptions,
+    htmlFor: radioOptions.id,
+    value: props.label,
+  })
+
+  return (
+    <label {...radioProps.radioContainer}>
+      <input {...radioProps.input} onChange={props.onChange} />
+      <span {...radioProps.radioControl} />
+      <span {...labelProps}>{value}</span>
+    </label>
+  )
+}
