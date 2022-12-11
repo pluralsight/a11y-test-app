b3f45d17124c307f0ef6864f819308424256cfb2
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-11 12:34:37 -0500

    fix: expand router to cover entire content and add page template

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,30 +1,8 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/routes'
-import AppBar from '../components/AppBar'
-import SideBar from '../components/SideBar'
-import styles from './App.module.css'
-import Footer from '../components/Footer'
-import MainContentRegion from '../components/MainContentRegion'
 
 function App() {
-  return (
-    <article className={styles.app}>
-      <div className={styles.header}>
-        <AppBar />
-      </div>
-      <div className={styles.sidebar}>
-        <SideBar />
-      </div>
-      <main className={styles.main}>
-        <MainContentRegion>
-          <RouterProvider router={router} />
-        </MainContentRegion>
-      </main>
-      <div className={styles.footer}>
-        <Footer />
-      </div>
-    </article>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
