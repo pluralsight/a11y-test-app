1ec3cec00ece5ebb7414164e8607b123afb41e6d
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-11 11:23:23 -0500

    feat: add react router

--- src/components/MainContentRegion/index.tsx
+++ src/components/MainContentRegion/index.tsx
@@ -1,33 +1,6 @@
-import { getGridItemProps, getGridProps } from '@pluralsight/headless-styles'
-import AsideContent from '../AsideContent'
-import MainContent from '../MainContent'
+import { type PropsWithChildren } from 'react'
+import { getGridProps } from '@pluralsight/headless-styles'
 
-export default function MainContentRegion() {
-  const mainGridProps = getGridItemProps({ colSpan: 9 })
-  const asideGridProps = getGridItemProps({ colSpan: 3 })
-  const mainProps = {
-    ...mainGridProps,
-    style: {
-      ...mainGridProps.style,
-      minWidth: 0,
-    },
-  }
-  const asideProps = {
-    ...asideGridProps,
-    style: {
-      ...asideGridProps.style,
-      minWidth: 0,
-    },
-  }
-
-  return (
-    <div {...getGridProps({ cols: 12, gap: 32 })}>
-      <div {...mainProps}>
-        <MainContent />
-      </div>
-      <div {...asideProps}>
-        <AsideContent />
-      </div>
-    </div>
-  )
+export default function MainContentRegion(props: PropsWithChildren) {
+  return <div {...getGridProps({ cols: 12, gap: 32 })}>{props.children}</div>
 }
