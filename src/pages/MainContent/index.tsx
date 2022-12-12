9b215ccc6cd70c298873bf9148b783dca81654d7
Tom Pietrosanti <tom.pietrosanti@gmail.com>
2022-12-12 10:35:30 -0500

    feat: add admonition example to main home content

--- src/pages/MainContent/index.tsx
+++ src/pages/MainContent/index.tsx
@@ -3,6 +3,7 @@ import HistoryCard from '../../components/HistoryCard'
 import TechFoundationsCard from '../../components/TechFoundationsCard'
 import GetStartedCard from '../../components/GetStartedCard'
 import styles from './MainContent.module.css'
+import Admonition from '../../components/Admonition'
 
 export default function MainContent(props: PropsWithChildren) {
   const [showGetStarted, setShowGetStarted] = useState(true)
@@ -13,6 +14,13 @@ export default function MainContent(props: PropsWithChildren) {
 
   return (
     <div className={styles.mainContent}>
+      <Admonition
+        title={'Scheduled maintenance'}
+        description={
+          'We are currently undergoing scheduled maintenance. During this time you may experience brief interruptions.  We apologize for any inconvenience.'
+        }
+        showButton={true}
+      />
       <h1 className="psds-screenreader-only">Pluralsight Skills Home</h1>
       {showGetStarted && <GetStartedCard handleClose={hideGetStarted} />}
       <HistoryCard />
